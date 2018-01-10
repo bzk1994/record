7b3650328a44d8ca41928994014a383fec066eeb
张浩 <735296233@qq.com>
2018-01-10 22:38:05 +0800

    ```

--- 2018-1.js
+++ 2018-1.js
@@ -3,7 +3,7 @@
 ## 2018-1
 
 ————————————————————————————————————————
-
+```
 2018/1/10 下午10:34:02
 
 Use es6 in mongodb
@@ -17,9 +17,9 @@ http://blog.csdn.net/u012810020/article/details/54582051
 bluebirdjs
 
 http://bluebirdjs.com/docs/api/promisification.html
-
+```
 ————————————————————————————————————————
-
+```
 2018/1/9 下午2:34:40
 
 mogoose
@@ -29,9 +29,9 @@ http://mongoosejs.com/index.html
 Robo 3T
 
 http://blog.csdn.net/u012810020/article/details/54574921
-
+```
 ————————————————————————————————————————
-
+```
 2018/1/8 上午10:44:45
 
 MongoDB
@@ -41,9 +41,9 @@ http://www.runoob.com/mongodb/mongodb-connections.html
 http://www.mongoing.com/docs/aggregation.html
 
 http://www.mongodb.org.cn/	
-
+```
 ————————————————————————————————————————
-
+```
 2018/1/5 下午5:57:16
 
 Express
@@ -53,17 +53,17 @@ http://www.expressjs.com.cn/
 NUXT
 
 https://zh.nuxtjs.org/
-
+```
 ————————————————————————————————————————
-
+```
 2018/1/4 下午8:05:14
 
 简单熟悉node API
 
 http://www.runoob.com/nodejs/nodejs-module-system.html
-
+```
 ————————————————————————————————————————
-
+```
 2018/1/3 下午1:50:34
 
 移动端浏览器优化
@@ -77,5 +77,5 @@ https://juejin.im/post/5a461f006fb9a0450408358f?utm_source=gold_browser_extensio
 哔哩哔哩（B站）的前端之路
 
 https://juejin.im/post/5a473a2df265da431441098c?utm_medium=fe&utm_source=weixinqun
-
+```
 ————————————————————————————————————————
