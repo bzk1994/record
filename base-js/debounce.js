0a82e329b54d6cfd7f073b90ae2c3cd86a7d47a9
张浩 <735296233@qq.com>
2018-02-23 19:04:13 +0800

    debounce

--- base-js/debounce.js
+++ base-js/debounce.js
@@ -1,17 +1,71 @@
 // 函数去抖（debounce）
 
+// 模糊查询
 
-var debounce = function (idle, action) {
-  var last
+/**
+ *
+ * @param {Funtion} fn 
+ * @param {Number} delay 
+ * 
+ * @return {Function} 
+ */
+const debounce = function (fn, delay) {
+  let timer
+
   return function () {
-    var ctx = this, args = arguments
-    clearTimeout(last)
-    last = setTimeout(function () {
-      action.apply(ctx, args)
-    }, idle)
+    var context = this
+    var args = arguments
+
+    clearTimeout(timer)
+
+    timer = setTimeout(function () {
+      fn.apply(context, args)
+    }, delay)
   }
 }
 
-debounce(function () {
-  console.log()
-})
+// test
+window.onresize = debounce(() => {
+  console.log('risize')
+}, 500)
+
+
+// underscore.js
+
+/**
+ * 
+ * @param {Funtion} func 
+ * @param {Number} wait 
+ * @param {Boolean} immediate 
+ */
+
+let _ = {}
+
+_.debounce = function (func, wait, immediate) {
+  var timeout, result;
+
+  var later = function (context, args) {
+    timeout = null;
+    if (args) result = func.apply(context, args);
+  };
+
+  var debounced = restArgs(function (args) {
+    if (timeout) clearTimeout(timeout);
+    if (immediate) {
+      var callNow = !timeout;
+      timeout = setTimeout(later, wait);
+      if (callNow) result = func.apply(this, args);
+    } else {
+      timeout = _.delay(later, wait, this, args);
+    }
+
+    return result;
+  });
+
+  debounced.cancel = function () {
+    clearTimeout(timeout);
+    timeout = null;
+  };
+
+  return debounced;
+};
