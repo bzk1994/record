299d581b84cd184082b320d6f29e5184025e2821
张浩 <735296233@qq.com>
2018-03-13 23:49:12 +0800

    3-13

--- http/README.md
+++ http/README.md
@@ -1,9 +1,17 @@
 知识点
 
-1 什么是http?
+1 什么是http
 
 2 http用途
 
 3 http与https
 
 4 http的缓存
+
+5 cookie 与 session
+
+1.cookies存储在浏览器中,可在浏览器控制台下查看.按照域名,路经,存储时间等分权限存储的.
+2.session存储在服务器中,一般默认用文件方式存储在服务器,存储方式一般为类似json的方式.session存储原理很简单,即使服务器端语言不支持也可自己实现.
+3.session的使用需要客户端来发送识别,也就是session_id.这个session_id可以用cookies存储并发送,也可以直接附在url中传送.由于url传送会不太友好,现在一般用cookies传送.所以禁用cookies会有影响,就只能用url方式传送了.但cookies不是唯一的方式.
+3.现在一般使用cookies存储session_id和一些校验工作,主要数据都存储在session.这样容易控制和统计.但具体如何使用两者你可按照自己想法去做.
+5.session除了用文件方式存储,还可以存储在各种数据库,或者内存数据库,比如redis等中.建议不要存在mysql等数据库,因为session的读取写入很频繁,对数据库压力很大.建议存储在内存数据库中.
--- node/biying/main.js
+++ node/biying/main.js
@@ -1,29 +1,57 @@
 const fs = require('fs')
 const request = require('request')
 const path = require('path')
+const async = require('async')
 
-const imagesDir = path.join(__dirname, 'imagesDir')
+const imagesDir = 'imagesDir'
 const Bing = 'https://cn.bing.com'
-const url = 'https://cn.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&nc=1520691403044&pid=hp&scope=web&FORM=HDRSC1'
 
-
-fs.mkdir('./imagesDir', err => {
-  if (err) {
-    return console.error(err)
-  }
-  console.log('create imagesDir Success!')
-})
-
-request(url, (err, res, body) => {
-  if (err) {
-    console.error(err)
-  } else {
-    const images = JSON.parse(body).images[0]
-    console.log(`${Bing}${images.url}`)
-    console.log(images.copyright)
-    const arr = images.url.split('/')
-    const str = arr[arr.length - 1]
-    // request(`${Bing}${images.url}`).pipe(fs.createReadStream(imagesDir, images.copyright))
-    request(`${Bing}${images.url}`).pipe(fs.createWriteStream(path.join('./imagesDir', str)))
+fs.mkdir(imagesDir, err => {
+  if (!err) {
+    console.log(`create ${imagesDir} Success!`)
   }
 })
+
+let imagesArr = []
+
+const downLoad = (url, src) => {
+  request(`${Bing}${url}`).pipe(fs.createWriteStream(path.join(`./${imagesDir}`, src))).on('close', () => {
+    console.log(`${src} Down Load Success!`)
+  })
+}
+
+const getImages = url => {
+  request(url, (err, res, body) => {
+    if (err) {
+      console.log(err)
+    } else {
+      const images = JSON.parse(body).images[0]
+      const name = images.url.split('/').pop()
+      imagesArr.push(name)
+      downLoad(images.url, name)
+    }
+  })
+}
+
+console.log(imagesArr)
+
+
+// eventproxy控制并发数
+new Array(100).fill(0).map((item, i) => {
+  let url = `https://cn.bing.com/HPImageArchive.aspx?format=js&idx=${i}&n=1&nc=1520691403044&pid=hp&scope=web&FORM=HDRSC1`
+  getImages(url)
+})
+
+// let url = `https://cn.bing.com/HPImageArchive.aspx?format=js&idx=${-2}&n=1&nc=1520691403044&pid=hp&scope=web&FORM=HDRSC1`
+// imagesArr.push(url)
+// getImages(url)
+
+// console.log(imagesArr)
+// new Array(10).fill(1).map((item, i) => {
+//   let idx = 0
+//   let url = `https://cn.bing.com/HPImageArchive.aspx?format=js&idx=${idx}&n=1&nc=1520691403044&pid=hp&scope=web&FORM=HDRSC1`
+//   console.log(idx, url)
+//   getImages(url)
+// })
+
+
